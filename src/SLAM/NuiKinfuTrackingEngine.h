41c13bef58bad5d18951374d2780fe38d806afb4
Tingzhu Zhou <tingzhu.zhou@autodesk.com>
2017-02-24 17:33:26 +0800

    Add namespace NuiKinfuEngine.

--- src/SLAM/NuiKinfuTrackingEngine.h
+++ src/SLAM/NuiKinfuTrackingEngine.h
@@ -13,45 +13,48 @@ class NuiKinfuTracker;
 
 typedef Eigen::Vector3i Vector3i;
 
-class NuiKinfuTrackingEngine
+namespace NuiKinfuEngine
 {
-public:
-	NuiKinfuTrackingEngine(NuiTrackerConfig& tracerConfig, UINT nWidth, UINT nHeight, UINT nColorWidth, UINT nColorHeight);
-	NuiKinfuTrackingEngine();
-	~NuiKinfuTrackingEngine();
+	class NuiKinfuTrackingEngine
+	{
+	public:
+		NuiKinfuTrackingEngine(NuiTrackerConfig& tracerConfig, UINT nWidth, UINT nHeight, UINT nColorWidth, UINT nColorHeight);
+		NuiKinfuTrackingEngine();
+		~NuiKinfuTrackingEngine();
 
-	bool	isInit() const { return (m_pTracker ? true : false); }
-	void	initialize(const NuiTrackerConfig& tracerConfig, UINT nWidth, UINT nHeight, UINT nColorWidth, UINT nColorHeight);
-	bool	log(const std::string& fileName) const;
+		bool	isInit() const { return (m_pTracker ? true : false); }
+		void	initialize(const NuiTrackerConfig& tracerConfig, UINT nWidth, UINT nHeight, UINT nColorWidth, UINT nColorHeight);
+		bool	log(const std::string& fileName) const;
 
-	/** \brief Performs the tracker reset to initial  state. It's used if case of camera tracking fail.  */
-	void	reset(const Vector3f& translateBasis);
+		/** \brief Performs the tracker reset to initial  state. It's used if case of camera tracking fail.  */
+		void	reset(const Vector3f& translateBasis);
 
-	bool	RunTracking(UINT16* pDepths,
-		ColorSpacePoint* pDepthToColor,
-		UINT nPointNum,
-		const NuiColorImage& image,
-		NuiKinfuScene*	pVolume,
-		const NuiCameraParams& cameraParams);
+		bool	RunTracking(UINT16* pDepths,
+			ColorSpacePoint* pDepthToColor,
+			UINT nPointNum,
+			const NuiColorImage& image,
+			NuiKinfuScene*	pVolume,
+			const NuiCameraParams& cameraParams);
 
-	/** \brief Returns camera pose at given time, default the last pose
-        * \param[in] time Index of frame for which camera pose is returned.
-        * \return camera pose
-        */
-    const NuiCameraPos&		getCameraPose (int time = -1) const;
-	float					getTrackerError() const;
-	float					getTrackerCount() const;
-	void					setIntegrationMetricThreshold(float threshold) { m_integration_metric_threshold = threshold; }
+		/** \brief Returns camera pose at given time, default the last pose
+			* \param[in] time Index of frame for which camera pose is returned.
+			* \return camera pose
+			*/
+		const NuiCameraPos&		getCameraPose (int time = -1) const;
+		float					getTrackerError() const;
+		float					getTrackerCount() const;
+		void					setIntegrationMetricThreshold(float threshold) { m_integration_metric_threshold = threshold; }
 
-	bool					previousBufferToData(NuiCLMappableData* pCLData);
-	bool					previousNormalImageToData(NuiCLMappableData* pCLData);
+		bool					previousBufferToData(NuiCLMappableData* pCLData);
+		bool					previousNormalImageToData(NuiCLMappableData* pCLData);
 
-private:
-	NuiKinfuTracker*		m_pTracker;
-	NuiKinfuFrame*			m_pFrame;
-	NuiKinfuCameraState*	m_pCameraState;
-	std::vector<NuiCameraPos> m_poses;
+	private:
+		NuiKinfuTracker*		m_pTracker;
+		NuiKinfuFrame*			m_pFrame;
+		NuiKinfuCameraState*	m_pCameraState;
+		std::vector<NuiCameraPos> m_poses;
 
-	NuiCameraPos			m_lastIntegrationPos;
-	float					m_integration_metric_threshold;
-};
+		NuiCameraPos			m_lastIntegrationPos;
+		float					m_integration_metric_threshold;
+	};
+}
