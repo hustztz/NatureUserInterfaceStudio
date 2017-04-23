e64da1c8661c2d13347d74c802c1c39fad605de0
Tingzhu Zhou <ztz_mai@163.com>
2017-04-23 21:17:33 +0800

    Add NuiLogger.

---
+++ src/Foundation/NuiLogger.cpp
@@ -0,0 +1,64 @@
+#include "NuiLogger.h"
+
+#include <log4cplus/initializer.h>
+#include <log4cplus/configurator.h>  
+#include <log4cplus/consoleappender.h>
+#include <log4cplus/fileappender.h>
+#include <log4cplus/helpers/loglog.h>
+
+#define LOG4CPLUS_CONF_FILE "./log4cplus.properties"
+
+using namespace log4cplus;
+using namespace log4cplus::helpers;
+
+NuiLogger& NuiLogger::instance() {
+	static NuiLogger instance;
+	return instance;
+}
+
+NuiLogger::NuiLogger()
+	: m_consoleAppender(new ConsoleAppender())
+	, m_fileAppender(new RollingFileAppender(LOG4CPLUS_TEXT("a////b/c/d/Test.log"), 5 * 1024, 5,
+		false, true))
+{
+	log4cplus::Initializer initializer;
+
+	//PropertyConfigurator::doConfigure(LOG4CPLUS_TEXT(LOG4CPLUS_CONF_FILE));
+#ifdef _DEBUG
+	LogLog::getLogLog()->setInternalDebugging(true);
+#endif // _DEBUG
+	
+	m_fileAppender->setName(LOG4CPLUS_TEXT("fileAppender"));
+	log4cplus::tstring pattern = LOG4CPLUS_TEXT("%d{%m/%d/%y %H:%M:%S,%Q} [%t] %-5p %c{2} %%%x%% - %X{key} - %m [%l]%n");
+	m_fileAppender->setLayout(std::unique_ptr<Layout>(new PatternLayout(pattern)));
+
+	//m_fileAppender->addFilter(spi::FilterPtr(new FunctionFilter(filterFunction)));
+
+	m_consoleAppender->setName(LOG4CPLUS_TEXT("consoleAppender"));
+	m_consoleAppender->setLayout(std::unique_ptr<Layout>(new TTCCLayout()));
+
+	Logger root = Logger::getRoot();
+	LOG4CPLUS_DEBUG(root,
+		"This is"
+		<< " a reall"
+		<< "y long message." << std::endl
+		<< "Just testing it out" << std::endl
+		<< "What do you think?");
+
+	m_consolelogger = Logger::getInstance(LOG4CPLUS_TEXT("consoleLogger"));
+	m_consolelogger.addAppender(m_consoleAppender);
+
+	m_filelogger = Logger::getInstance(LOG4CPLUS_TEXT("fileLogger"));
+	m_filelogger.addAppender(m_fileAppender);
+	m_filelogger.setLogLevel(INFO_LOG_LEVEL);
+}
+
+NuiLogger::~NuiLogger()
+{
+}
+
+void	NuiLogger::setQuietMode()
+{
+	LogLog::getLogLog()->setQuietMode(true);
+}
+
---
+++ src/Foundation/NuiLogger.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <log4cplus/logger.h>
+#include <log4cplus/loggingmacros.h>
+
+// LOG4CPLUS_DEBUG
+// LOG4CPLUS_INFO
+// LOG4CPLUS_WARN
+// LOG4CPLUS_ERROR
+// LOG4CPLUS_FATAL
+class NuiLogger
+{
+public:
+	
+	static NuiLogger& instance();
+
+	void	setQuietMode();
+
+	const log4cplus::Logger& consoleLogger() const { return m_consolelogger; }
+	const log4cplus::Logger& fileLogger() const { return m_filelogger; }
+
+private:
+	log4cplus::Logger				m_consolelogger;
+	log4cplus::SharedAppenderPtr	m_consoleAppender;
+	log4cplus::Logger				m_filelogger;
+	log4cplus::SharedAppenderPtr	m_fileAppender;
+
+private:
+	NuiLogger();
+	~NuiLogger();
+};
